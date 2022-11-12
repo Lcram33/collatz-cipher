f2dde2463131fa4603722a71b46faf57d2505463
Lcram33 <43505358+Lcram33@users.noreply.github.com>
2022-11-13 00:56:52 +0100

    Delete params_default.py

--- params_default.py
+++
@@ -1,8 +0,0 @@
-#extracted from the string library : string.printable, with some minor changes. You can add yours ! Be careful with the \ char, however,
-#and some other chars that may be used by Python, causing some errors/unexpected behaviour.
-DEFAULT_CHARSET = """0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-ùéèêàâôîïçÉ !"#$%&'()*+,-./:;<=>?@[]^_`{|}~"""
-
-
-#just used to confuse cryptanalysts :)
-UNUSED_CHARS = "αβΓΔδεζηΘ"
